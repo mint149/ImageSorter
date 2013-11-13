e455698f099771d25e5d0e82fac74ecadd1b4473
mint <skylove.exe@gmail.com>
2013-11-13 23:44:35 +0900

    ソースの上部コメント変更

--- src/ImageSorter.cs
+++ src/ImageSorter.cs
@@ -1,8 +1,5 @@
-/*
-画像整理プログラムImageSorter
-･ASDFJKL;にフォルダを割り当て，押した瞬間に見てる画像をフォルダに移動，次の画像を表示する．TODO
-･アンドゥ対応TODO
-*/
+//画像整理プログラムImageSorter
+
 using System;
 using System.Collections.Generic;
 using System.IO;
